35aeb0227873c814d53bb709dbf84b455fadd2b1
Will Saults <wsaults@yahoo.com>
2019-04-02 17:07:01 -0500

    Added router-outlet and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MainPageComponent},
+  { path: 'about', component: AboutUsComponent },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    MainPageComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes
+    ),
     BrowserModule
   ],
   providers: [],
